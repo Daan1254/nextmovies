0f4503a7ac7cafd30acd775e9af637faaf79f953
Quin Coolen <quin.coolen@gmail.com>
2023-06-29 15:25:23 +0200

    fix meta tags

--- front-end/pages/_document.tsx
+++ front-end/pages/_document.tsx
@@ -5,8 +5,67 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta property="og:title" content="My page title" key="title" />
-        <meta name="description" content="The best cinema to view movies in!" />
+        <meta name="application-name" content="NextMovies" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="NextMovies" />
+        <meta name="description" content="Best place to watch movies!" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/touch-icon-ipad.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/touch-icon-iphone-retina.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/icons/touch-icon-ipad-retina.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Next Movies" key="title" />
+        <meta property="og:description" content="The best cinema to view movies in!" />
+        <meta property="og:site_name" content="NextMovies" />
+        <meta property="og:url" content="https://nextmovies-three.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://yourdomain.com/icons/apple-touch-icon.png"
+        />
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
