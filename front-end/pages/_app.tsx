9a2d044dab0222f088db969a38b2845944dd3e4a
Quin Coolen <quin.coolen@gmail.com>
2023-10-05 12:57:09 +0200

    Update _app.tsx

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
-export const databaseUrl = "nextmovies-production-62e8.up.railway.app";
+export const databaseUrl = "nextmovies-production-62e8.up.railway.app/api";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
